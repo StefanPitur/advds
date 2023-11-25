8cc381ce13b2dfcb7c9802556e05d658f0cf23fb
Stefan - Alexandru Pitur <stefan.pitur24@gmail.com>
2023-11-25 20:55:03 +0000

    implemented pp_data_table creation via schema

--- fynesse/access.py
+++ fynesse/access.py
@@ -71,13 +71,57 @@ class PropertyPricesDbConnector:
 
 def create_and_populate_pp_data_table(property_prices_db: PropertyPricesDbConnector):
     create_pp_data_table(property_prices_db)
-    conn = property_prices_db.get_conn()
+    populate_pp_data_table(property_prices_db)
 
     return "Calling method to generate pp_data table"
 
 
 def create_pp_data_table(property_prices_db: PropertyPricesDbConnector):
     conn = property_prices_db.get_conn()
+    try:
+        conn.cursor().execute(f"""
+                --
+                -- Table structure for table `pp_data`
+                --
+                DROP TABLE IF EXISTS `pp_data`;
+                CREATE TABLE IF NOT EXISTS `pp_data` (
+                  `transaction_unique_identifier` tinytext COLLATE utf8_bin NOT NULL,
+                  `price` int(10) unsigned NOT NULL,
+                  `date_of_transfer` date NOT NULL,
+                  `postcode` varchar(8) COLLATE utf8_bin NOT NULL,
+                  `property_type` varchar(1) COLLATE utf8_bin NOT NULL,
+                  `new_build_flag` varchar(1) COLLATE utf8_bin NOT NULL,
+                  `tenure_type` varchar(1) COLLATE utf8_bin NOT NULL,
+                  `primary_addressable_object_name` tinytext COLLATE utf8_bin NOT NULL,
+                  `secondary_addressable_object_name` tinytext COLLATE utf8_bin NOT NULL,
+                  `street` tinytext COLLATE utf8_bin NOT NULL,
+                  `locality` tinytext COLLATE utf8_bin NOT NULL,
+                  `town_city` tinytext COLLATE utf8_bin NOT NULL,
+                  `district` tinytext COLLATE utf8_bin NOT NULL,
+                  `county` tinytext COLLATE utf8_bin NOT NULL,
+                  `ppd_category_type` varchar(2) COLLATE utf8_bin NOT NULL,
+                  `record_status` varchar(2) COLLATE utf8_bin NOT NULL,
+                  `db_id` bigint(20) unsigned NOT NULL
+                ) DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;
+            """)
+
+        conn.cursor().execute(f"""
+                --
+                -- Primary key for table `pp_data`
+                --
+                ALTER TABLE `pp_data`
+                ADD PRIMARY KEY (`db_id`);
+
+                ALTER TABLE `pp_data`
+                MODIFY db_id bigint(20) unsigned NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+            """)
+    except Exception as e:
+        raise Exception("Could not create `pp_data` table on the database server - {}".format(e))
+
+
+def populate_pp_data_table(property_prices_db: PropertyPricesDbConnector):
+    pass
+
 
 def data():
     """Read the data from the web or local file, returning structured format such as a data frame"""
