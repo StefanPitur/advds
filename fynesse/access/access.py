ba33566aae3f993dcf37d80811efe85df63d4087
Stefan - Alexandru Pitur <stefan.pitur24@gmail.com>
2023-11-27 19:39:43 +0000

    moved PostcodeData code from class to address.py

--- fynesse/access/access.py
+++ fynesse/access/access.py
@@ -17,6 +17,8 @@ both intellectual property and personal data privacy rights. Beyond the legal si
 about the ethical issues around this data. 
 """
 
+# Create property_prices database connection on remote SQL server
+
 
 def create_property_prices_database(host, port, username, password):
     conn = pymysql.connect(
@@ -38,6 +40,9 @@ def create_property_prices_db_connection(host, port, username, password):
         local_infile=1
     )
     return conn
+
+
+# pp_data code
 
 
 def create_and_populate_pp_data_table(conn):
@@ -99,4 +104,69 @@ def create_column_index_on_pp_data_table(conn, column_name):
     conn.cursor().execute(f"""
         CREATE INDEX IF NOT EXISTS `{index_column_name}` USING HASH
         ON `pp_data` ({column_name})
+    """)
+
+
+# postcode_data code
+
+
+def create_and_populate_postcode_data_table(conn):
+    create_postcode_data_table(conn)
+    populate_postcode_data_table(conn)
+
+
+def create_postcode_data_table(conn):
+    conn.cursor().execute("""
+        -- Table structure for table `postcode_data`
+        DROP TABLE IF EXISTS `postcode_data`;
+    """)
+    conn.cursor().execute("""
+        CREATE TABLE IF NOT EXISTS `postcode_data` (
+          `postcode` varchar(8) COLLATE utf8_bin NOT NULL,
+          `status` enum('live','terminated') NOT NULL,
+          `usertype` enum('small', 'large') NOT NULL,
+          `easting` int unsigned,
+          `northing` int unsigned,
+          `positional_quality_indicator` int NOT NULL,
+          `country` enum('England', 'Wales', 'Scotland', 'Northern Ireland', 'Channel Islands', 'Isle of Man') NOT NULL,
+          `latitude` decimal(11,8) NOT NULL,
+          `longitude` decimal(10,8) NOT NULL,
+          `postcode_no_space` tinytext COLLATE utf8_bin NOT NULL,
+          `postcode_fixed_width_seven` varchar(7) COLLATE utf8_bin NOT NULL,
+          `postcode_fixed_width_eight` varchar(8) COLLATE utf8_bin NOT NULL,
+          `postcode_area` varchar(2) COLLATE utf8_bin NOT NULL,
+          `postcode_district` varchar(4) COLLATE utf8_bin NOT NULL,
+          `postcode_sector` varchar(6) COLLATE utf8_bin NOT NULL,
+          `outcode` varchar(4) COLLATE utf8_bin NOT NULL,
+          `incode` varchar(3)  COLLATE utf8_bin NOT NULL,
+          `db_id` bigint(20) unsigned NOT NULL
+        ) DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+    """)
+    conn.cursor().execute("""
+        ALTER TABLE `postcode_data`
+        ADD PRIMARY KEY (`db_id`);
+    """)
+    conn.cursor().execute("""
+        ALTER TABLE `postcode_data`
+        MODIFY `db_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=1;
+    """)
+
+
+def populate_postcode_data_table(conn):
+    conn.cursor().execute("""
+        LOAD DATA LOCAL INFILE 'open_postcode_geo.csv' INTO TABLE `postcode_data`
+        FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED by '"'
+        LINES STARTING BY '' TERMINATED BY '\n';
+    """)
+
+
+def create_column_index_on_postcode_data_table(conn, column_name):
+    index_column_name = "pd." + column_name
+
+    conn.cursor().execute(f"""
+        DROP INDEX IF EXISTS `{index_column_name}` ON `postcode_data`
+    """)
+    conn.cursor().execute(f"""
+        CREATE INDEX IF NOT EXISTS `{index_column_name}` USING HASH
+        ON `postcode_data` ({column_name})
     """)
